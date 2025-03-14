164c3594c2bd60d5acb102ed1488be64b9064a3a
ciccio <116078751+dariuccio04@users.noreply.github.com>
2025-03-14 11:43:44 +0100

    Update pigreco.m

--- pigreco.m
+++ pigreco.m
@@ -9,7 +9,7 @@ u(1) = 1;
 s(2) = 1.25;  %1 + 1/4;
 u(2) = s(2);
 %itero fino a 40
-for n=2:40
+for n=2:200
     s(n+1) = s(n)+(n+1)^(-2);
     u(n+1) = sqrt(6*s(n+1));
 end
@@ -25,7 +25,7 @@ fprintf('\n');
 format long %maggiore accuratezza
 z(1)=1;
 z(2)=2;
-for n=2:40
+for n=2:10000
     c=(4^(1-n))*(z(n))^2; %ultima parte dentro le radici
     inner_sqrt=sqrt(1-c); %radice interna 
     z(n+1) = (2^(n-0.5))*sqrt(1-inner_sqrt); 
@@ -34,3 +34,37 @@ for n=2:40
 end
 rel_err_z=abs(z-pi)/pi;
 fprintf('\n')
+
+
+
+
+
+%alg3 (simil alg2?)
+y(1)=1;
+y(2)=2;
+for n=2:200
+    num=(2^(1/2)) * abs(y(n));
+    %sqrt(2) * abs(y(n));
+    c=(4^(1-n)) *(y(n))^2;
+    inner_sqrt=sqrt(1-c);
+    %fin ora ho: sqrt(1-(4^(1-n)) * (y(n))^2
+    den = sqrt(1+inner_sqrt);
+    %ora ho: sqrt(1 + sqrt(1-(4^(1-n)) * (y(n))^2)
+    y(n+1)=num/den;
+end
+%num -> deriva dalla riscrittura della formula originale per semplificare divisioni e radici quadrate, garantendo stabilità numerica durante le iterazioni.
+rel_err_y=abs(y-pi)/pi;
+
+%plot semilogy
+semilogy(1:length(u), rel_err_u, 'k.'); %asse y in log e asse x in scala lineare
+hold on; %hold on serve per mantenere il grafico attuale e permettere di aggiungere nuovi elementi senza cancellarlo
+semilogy(1:length(z),rel_err_z, 'm+');
+semilogy(1:length(y),rel_err_y,'ro');
+hold off;
+
+
+%Un errore di 10^-15 indica che l'algoritmo ha raggiunto il limite fisico della precisione numerica del calcolatore, mentre un errore di 
+%10^0 segnala un fallimento totale dovuto a instabilità. La differenza sta nella capacità dell'algoritmo di controllare gli errori anziché lasciarli dominare.
+
+%quindi l'alg 3 da il migliore risultato -> sistemato dal punto di vista di
+%cancellazione numerica
